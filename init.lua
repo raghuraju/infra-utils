f65c682609ab5e3b4466a4c278dba9c6a340a331
raghu <932416+raghuraju@users.noreply.github.com>
2026-04-07 06:23:12 +0530

    Vim config - work in progress

--- init.lua
+++ init.lua
@@ -83,4 +83,336 @@ vim.opt.redrawtime = 10000
 vim.opt.maxmempattern = 20000 -- increase max memory
 
 -- STATUS LINE
+local cached_branch = ""
+local last_check = 0
+local function git_branch()
+  local now = vim.loop.now()
+  if now - last_check > 5000 then
+    cached_branch = vim.fn.system("git branch --show-current 2>/dev/null | tr -d '\n'")
+    last_check = now
+  end
+  if cached_branch ~= "" then
+    return " \u{e725} " .. cached_branch .. " " -- git branch
+  end
+  return ""
+end
 
+-- FILE TYPE WITH NERD FONT ICONS
+local function file_type()
+  local ft = vim.bo.filetype
+  local icons = {
+    lua = "\u{e620} ", -- nf-dev-lua
+		python = "\u{e73c} ", -- nf-dev-python
+		javascript = "\u{e74e} ", -- nf-dev-javascript
+		typescript = "\u{e628} ", -- nf-dev-typescript
+		javascriptreact = "\u{e7ba} ",
+		typescriptreact = "\u{e7ba} ",
+		html = "\u{e736} ", -- nf-dev-html5
+		css = "\u{e749} ", -- nf-dev-css3
+		scss = "\u{e749} ",
+		json = "\u{e60b} ", -- nf-dev-json
+		markdown = "\u{e73e} ", -- nf-dev-markdown
+		vim = "\u{e62b} ", -- nf-dev-vim
+		sh = "\u{f489} ", -- nf-oct-terminal
+		bash = "\u{f489} ",
+		zsh = "\u{f489} ",
+		rust = "\u{e7a8} ", -- nf-dev-rust
+		go = "\u{e724} ", -- nf-dev-go
+		c = "\u{e61e} ", -- nf-dev-c
+		cpp = "\u{e61d} ", -- nf-dev-cplusplus
+		java = "\u{e738} ", -- nf-dev-java
+		php = "\u{e73d} ", -- nf-dev-php
+		ruby = "\u{e739} ", -- nf-dev-ruby
+		swift = "\u{e755} ", -- nf-dev-swift
+		kotlin = "\u{e634} ",
+		dart = "\u{e798} ",
+		elixir = "\u{e62d} ",
+		haskell = "\u{e777} ",
+		sql = "\u{e706} ",
+		yaml = "\u{f481} ",
+		toml = "\u{e615} ",
+		xml = "\u{f05c} ",
+		dockerfile = "\u{f308} ", -- nf-linux-docker
+		gitcommit = "\u{f418} ", -- nf-oct-git_commit
+		gitconfig = "\u{f1d3} ", -- nf-fa-git
+		vue = "\u{fd42} ", -- nf-md-vuejs
+		svelte = "\u{e697} ",
+		astro = "\u{e628} ",
+  }
+
+  if ft == "" then
+    return " \u{f15b} " -- na-fa-file_o
+  end
+
+  return ((icons[ft] or " \u{f15b} ") .. ft)
+end
+
+-- File size with nerd font icon
+local function file_size()
+  local size = vim.fn.getfsize(vim.fn.expand("%"))
+  if size < 0 then
+    return ""
+  end
+
+  if size < 1024 then
+    size_str = size .. "B"
+  elseif size < 1024 * 1024 then
+    size_str = string.format("%.1fK", size / 1024 )
+  else
+    size_str = string.format("%.1fM", size / 1024 / 1024 )
+  end
+
+  return " \u{f016} " .. size_str .. ""
+end
+
+-- mode with nerd font icons
+local function mode_icon()
+  local mode = vim.fn.mode()
+  local modes = {
+    n = " \u{f121}  NORMAL",
+		i = " \u{f11c}  INSERT",
+		v = " \u{f0168} VISUAL",
+		V = " \u{f0168} V-LINE",
+		["\22"] = " \u{f0168} V-BLOCK",
+		c = " \u{f120} COMMAND",
+		s = " \u{f0c5} SELECT",
+		S = " \u{f0c5} S-LINE",
+		["\19"] = " \u{f0c5} S-BLOCK",
+		R = " \u{f044} REPLACE",
+		r = " \u{f044} REPLACE",
+		["!"] = " \u{f489} SHELL",
+		t = " \u{f120} TERMINAL",
+  }
+  return modes[mode] or (" \u{f059} " .. mode)
+end
+
+_G.mode_icon = mode_icon
+_G.git_branch = git_branch
+_G.file_type = file_type
+_G.file_size = file_size
+
+vim.cmd([[
+  highlight StatusLineBold gui=bold cterm=bold
+]])
+
+-- Function to change status line based on window focus
+local function setup_dynamic_statusline()
+  vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+    callback = function()
+      vim.opt_local.statusline = table.concat({
+        " ",
+        "%#StatusLineBold#",
+				"%{v:lua.mode_icon()}",
+				"%#StatusLine#",
+				" \u{e0b1} %f %h%m%r", -- nf-pl-left_hard_divider
+				"%{v:lua.git_branch()}",
+				"\u{e0b1} ", -- nf-pl-left_hard_divider
+				"%{v:lua.file_type()}",
+				"\u{e0b1} ", -- nf-pl-left_hard_divider
+				"%{v:lua.file_size()}",
+				"%=", -- Right-align everything after this
+				" \u{f017} %l:%c  %P ", -- nf-fa-clock_o for line/col
+      })
+    end,
+  })
+
+  vim.api.nvim_set_hl(0, "StatusLineBold", { bold = true })
+
+  vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+    callback = function()
+      vim.opt_local.statusline = "  %f %h%m%r \u{e0b1} %{v:lua.file_type()} %=  %l:%c   %P "
+    end,
+  })
+end
+
+setup_dynamic_statusline()
+
+-- KEYMAPS
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "j", function()
+  return vim.v.count == 0 and "gj" or "j"
+end, { expr = true, silent = true, desc = "Down (wrap aware)"})
+vim.keymap.set("n", "k", function()
+  return vim.v.count == 0 and "gk" or "k"
+end, { expr = true, silent = true, desc = "Up (wrap aware)"})
+
+vim.keymap.set("n", "<leader>1", ":e config/init.lua<CR>", { desc = "Temporary shortcut to open the init file" })
+vim.keymap.set("n", "<leader>c", ":nohlsearch<CR>", { desc = "Clear highlight search" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Hald page down (centered)." })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)." })
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste without yanking." })
+vim.keymap.set({"n", "v"}, "<leader>x", '"_d', { desc = "Delted without yanking." })
+
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left window." })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to the bottom window."})
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to the top window." })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right window." })
+
+vim.keymap.set("n", "<leader>w", ":wq<CR>", { desc = "Write and close buffer/tab" }) -- my own keymap!
+vim.keymap.set("n", "<leader>s", ":w<CR>", { desc = "Save the file" }) -- my own
+vim.keymap.set("n", "<leader>o", "Go", { desc = "Append at the end of the file." }) -- my own
+vim.keymap.set("n", "QQ", ":q<CR>", { desc = "Quick exit!" })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically." })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally." })
+
+-- behaviour not as expected --
+-- vim.keymap.set("n", "<cmd-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+-- vim.keymap.set("n", "<cmd-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+-- vim.keymap.set("n", "<cmd-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+-- vim.keymap.set("n", "<cmd-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+-- further study is needed --
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down"})
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+
+vim.keymap.set("n", "<leader>pa", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print("file path: ", path)
+end, { desc = "Copy full file path" })
+
+vim.keymap.set("n", "<leader>td", function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "Toggle diagnostics" })
+
+-- AUTOCOMMANDS
+
+local augroup = vim.api.nvim_create_augroup("UserConfig", { clear = true })
+
+-- highlight text on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- return to the last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup,
+  desc = "Return to the last cursor position",
+  callback = function()
+    if vim.o.diff then
+      return
+    end
+
+    local last_pos = vim.api.nvim_buf_get_mark(0, '"') -- {line, col}
+    local last_line = vim.api.nvim_buf_line_count(0)
+
+    local row = last_pos[1]
+    if row < 1 or row > last_line then
+      return
+    end
+
+    pcall(vim.api.nvim_win_set_cursor, 0, last_pos)
+  end,
+})
+
+-- wrap, linebreak and spell check on markdown and text files
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = {"markdown", "text", "gitcommit"},
+  callback = function()
+   vim.opt_local.wrap = true
+   vim.opt_local.linebreak = true
+   vim.opt_local.spell = true
+  end,
+})
+
+-- plugins
+vim.pack.add({
+  "https://www.github.com/nvim-tree/nvim-tree.lua",
+  "https://www.github.com/nvim-tree/nvim-web-devicons",
+  "https://www.github.com/ibhagwan/fzf-lua",
+  "https://www.github.com/nvim-mini/mini.nvim",
+  "https://www.github.com/lewis6991/gitsigns.nvim",
+})
+
+local function packadd(name)
+  vim.cmd("packadd " .. name)
+end
+
+packadd("nvim-tree.lua")
+packadd("fzf-lua")
+packadd("nvim-web-devicons")
+packadd("mini.nvim")
+packadd("gitsigns.nvim")
+
+-- plugin config
+
+require("nvim-tree").setup({
+  view = {
+    width = 35,
+  },
+  filters = {
+    dotfiles = false,
+  },
+  renderer = {
+    group_empty = true,
+  },
+})
+vim.keymap.set("n", "<leader>e", function()
+  require("nvim-tree.api").tree.toggle()
+end, { desc = "Toggle Nvim Tree" })
+
+require("fzf-lua").setup({})
+
+vim.keymap.set("n", "<leader>ff", function()
+  require("fzf-lua").files()
+end, { desc = "FZF Files" })
+vim.keymap.set("n", "<leader>fg", function()
+  require("fzf-lua").live_grep()
+end, { desc = "FZF Live Grep" })
+vim.keymap.set("n", "<leader>fb", function()
+  require("fzf-lua").buffers()
+end, { desc = "FZF Buffers"})
+vim.keymap.set("n", "<leader>fh", function()
+  require("fzf-lua").help_tags()
+end, { desc = "FZF Help Tags" })
+vim.keymap.set("n", "<leader>fx", function()
+  require("fzf-lua").diagnostics_document()
+end, { desc = "FZF Diagnostics Document" })
+vim.keymap.set("n", "<leader>fX", function()
+  require("fzf-lua").diagnostics_workspace()
+end, { desc = "FZF Diagnostics Workspace" })
+
+require("nvim-web-devicons").setup({})
+
+require("mini.ai").setup({})
+require("mini.comment").setup({})
+require("mini.move").setup({})
+require("mini.surround").setup({})
+require("mini.cursorword").setup({})
+require("mini.indentscope").setup({})
+require("mini.pairs").setup({})
+require("mini.trailspace").setup({})
+require("mini.bufremove").setup({})
+require("mini.notify").setup({})
+require("mini.icons").setup({})
+
+require("gitsigns").setup({
+  signs = {
+    add = { text = "\u{2590}" }, -- ▏
+		change = { text = "\u{2590}" }, -- ▐
+		delete = { text = "\u{2590}" }, -- ◦
+		topdelete = { text = "\u{25e6}" }, -- ◦
+		changedelete = { text = "\u{25cf}" }, -- ●
+		untracked = { text = "\u{25cb}" }, -- ○
+  },
+  signcolumn = true,
+  -- current_column_blame = false,
+})
